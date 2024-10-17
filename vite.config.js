bd1a23baf4974e306e96ab8c0277e31362d4202f
Ramon Jardí <risenjardi@gmail.com>
2024-10-17 16:27:43 +0200

    Github pages deploy

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/css-elearning-landing-page/', // Reemplaza 'nombre-del-repo' por el nombre exacto de tu repositorio
+})
